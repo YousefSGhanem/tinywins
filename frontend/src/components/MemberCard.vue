e79802bb24ee55e1f3f0051ec6722a372a6c2e48
Yousef Ghanem <Yousef.s.ghanem@gmail.com>
2026-02-22 15:41:33 +0100

    Add reusble MemberCrd component with mock data

---
+++ frontend/src/components/MemberCard.vue
@@ -0,0 +1,19 @@
+<template>
+  <v-card :color="color" class="pa-4 text-center" elvation="4">
+    <v-avatar size="64" class="mb-3">
+      <v-icon size="40">mdi-account</v-icon>
+    </v-avatar>
+    <div class="text-h6 font-weight-bold">
+      {{name}}
+    </div>
+  </v-card>
+</template>
+
+<script setup lang="ts">
+
+defineProps<{
+  name: string,
+  color: string
+}>()
+
+</script>
--- frontend/src/views/MembersView.vue
+++ frontend/src/views/MembersView.vue
@@ -1,5 +1,25 @@
 <template>
-  <v-text-field>
-    Hallo
-  </v-text-field>
+  <v-row>
+    <v-col
+      v-for="member in members"
+      :key="member.name"
+      cols="12"
+      sm="6"
+      md="4">
+      <MemberCard :name="member.name" :color="member.color"/>
+    </v-col>
+  </v-row>
 </template>
+
+
+<script setup lang="ts">
+import MemberCard from '../components/MemberCard.vue'
+
+const members = [
+  {name: 'Zayneb' , color: 'secondary'},
+  {name: 'Jamal' , color: 'primary'},
+  {name: 'Basma' , color: 'accent'},
+
+]
+
+</script>
