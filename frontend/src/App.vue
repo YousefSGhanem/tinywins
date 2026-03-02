e069df648ee19b698bd096a089fae91d5adceba5
Yousef Ghanem <Yousef.s.ghanem@gmail.com>
2026-03-02 02:32:31 +0100

    Add parent PIN Dialog to unlock parent mode

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -2,8 +2,8 @@
   <v-app style="background-color: #F7F8FA; color: #2E2E2E;">
     <v-app-bar title="TinyWins">
       <template #append>
-        <v-btn variant="tonal" color="primary" @click="toggleParentMode">
-          {{ parentModeLabel}}
+        <v-btn variant="tonal" color="primary" @click="onModeButtonClick">
+          {{ modeLabel}}
         </v-btn>
       </template>
     </v-app-bar>
@@ -13,19 +13,72 @@
         <router-view/>
       </v-container>
     </v-main>
+
+    <v-dialog v-model="pinDialogOpen" max-width="360">
+      <v-card>
+        <v-card-title>Parent PIN</v-card-title>
+        <v-card-text>
+          <v-text-field
+            v-model="pinInput"
+            label="Enter 4-digit PIN"
+            type="password"
+            inputmode="numeric"
+            maxlength="4"
+            hint="Only Parents should know this."
+            persistent-hint
+            :error-messages="pinError"
+          />
+        </v-card-text>
+        <v-card-actions>
+          <v-spacer/>
+            <v-btn variant="text" @click="cancelPin">Cancel</v-btn>
+            <v-btn color="primary" variant="flat" @click="confirmPin">Unlock</v-btn>
+        </v-card-actions>
+      </v-card>
+    </v-dialog>
   </v-app>
 </template>
 
 
 <script setup lang="ts">
-import { computed} from 'vue'
+import {computed, ref} from 'vue'
 import {appState} from './state/appState'
 
-const parentModeLabel = computed(() => appState.isParentMode ? 'Parent Mode' : 'Kid Mode')
+const modeLabel = computed(() => appState.isParentMode ? 'Parent Mode' : 'Kid Mode')
 
-function toggleParentMode() {
-  //later we can use PIN
-  appState.isParentMode= !appState.isParentMode
+const pinDialogOpen = ref(false)
+const pinInput = ref('')
+const pinError = ref('')
+
+function onModeButtonClick() {
+  if (appState.isParentMode) {
+    appState.isParentMode= false
+    pinInput.value = ''
+    pinError.value = ''
+    return
+  }
+
+  // Kid -> Parent: Pin required
+  pinDialogOpen.value = true
+  pinInput.value = ''
+  pinError.value = ''
+}
+
+function confirmPin() {
+  if(pinInput.value === appState.parentPin) {
+    appState.isParentMode = true
+    pinDialogOpen.value = false
+    pinInput.value = ''
+    pinError.value = ''
+  } else {
+    pinError.value = 'Wrong PIN'
+  }
+}
+
+function cancelPin() {
+  pinDialogOpen.value = false
+  pinInput.value = ''
+  pinError.value = ''
 }
 
 </script>
--- frontend/src/state/appState.ts
+++ frontend/src/state/appState.ts
@@ -1,5 +1,7 @@
 import {reactive} from "vue"
 
 export const appState = reactive({
-  isParentMode: false
+  isParentMode: false,
+  // only for MVP (later i will do it with DB)
+  parentPin: '1234'
 })
